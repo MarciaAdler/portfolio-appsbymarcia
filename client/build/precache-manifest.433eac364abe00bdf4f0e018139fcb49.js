05958f6664e4c02f52aa741c784c0025231863a0
Marcia <marciaadler718@gmail.com>
2020-06-16 12:57:04 -0400

    ran npm run build

---
+++ client/build/precache-manifest.433eac364abe00bdf4f0e018139fcb49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031f9621bd216cd580bf1299dfa22ed0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd7053bcc79c377ed668",
+    "url": "/static/css/main.8ba3a25e.chunk.css"
+  },
+  {
+    "revision": "afc86fc01e368b187654",
+    "url": "/static/js/2.495f8649.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.495f8649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7053bcc79c377ed668",
+    "url": "/static/js/main.93769762.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "888e4cfd0f186594d3704452d909c3a8",
+    "url": "/static/media/backgroundimage.888e4cfd.jpg"
+  },
+  {
+    "revision": "16d51c9bf9ac3836bdc77948d27fdc5e",
+    "url": "/static/media/me.16d51c9b.jpeg"
+  }
+]);
