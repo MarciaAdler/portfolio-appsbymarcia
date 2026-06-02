4af868d83dcd15451af95669d56eec1784531b93
Marcia B Adler <marciabadler@marcias-air.mynetworksettings.com>
2026-06-02 16:50:59 -0400

    added ProductJobs app to portfolio page

---
+++ client/src/components/PortfolioItemsai.js
@@ -0,0 +1,122 @@
+import React from "react";
+import "./NavTabsstyle.css";
+const styles = {
+  images: {
+    height: "60px",
+    width: "auto",
+    borderTopLeftRadius: "20px",
+    borderTopRightRadius: "20px",
+    maxWidth: "100%",
+  },
+  background: {
+    opacity: "90%",
+    backgroundImage: "url(../images/blue-snow.png)",
+  },
+  content: {
+    color: "black",
+    height: "200px",
+  },
+  cardheader: {
+    borderBottom: "0",
+    padding: "0",
+    textAlign: "center",
+    maxWidth: "100%",
+    overflow: "hidden",
+    maxHeight: "60px",
+  },
+  card: {
+    height: "320px",
+    width: "365px",
+    backgroundColor: "#FFFBFC",
+    borderRadius: "30px",
+    overflow: "hidden",
+    
+  },  
+  tech: {
+    fontSize: "10px",
+  },
+  desc: {
+    fontSize: "12px",
+  },
+  footer: {
+    backgroundColor: "#f1f1f6",
+  },
+  link: {
+    // display: "inline-block",
+    // marginRight: "15px",
+    // fontSize: "13px",
+    // // backgroundColor: "#406e8e",
+    // borderRadius: ".2em",
+    // paddingLeft: "10px",
+    // paddingRight: "10px",
+    // paddingTop: "3px",
+    // paddingBottom: "3px",
+    // color: "#161925",
+  },
+};
+
+function PortfolioItemsai(props) {
+  return (
+    <div className="col-12 col-md-6 col-lg-4 d-flex justify-content-top align-items-center">
+      <div className="card bg-transparent border-dark mb-3" style={styles.card}>
+        <div
+          className="card-header bg-transparent py-0 px-0"
+          style={
+            (styles.cardheader,
+            {
+              backgroundImage: `url(${props.titleimage})`,
+              backgroundSize: "1000%",
+              borderTopRightRadius: "20px",
+              borderTopLeftRadius: "20px",
+              textAlign: "center",
+            })
+          }
+        >
+          <img style={styles.images} alt={props.name} src={props.titleimage} />
+        </div>
+
+        <div className="content card-body pt-1 pb-1" style={styles.background}>
+          <div style={styles.content}>
+            <h6 className="text-center mt-1">
+              <strong>{props.name}</strong>
+            </h6>
+            <p style={styles.desc}>{props.shortdescription}</p>
+            <p style={styles.tech}>
+              <i>Technologies used: {props.technologies}</i>
+            </p>
+            <p style={styles.tech}>
+              <i>Skills used: {props.skills}</i>
+            </p>
+          </div>
+        </div>
+        <div
+          className="card-footer d-flex justify-content-center"
+          style={styles.footer}
+        >
+          <a
+            href={props.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={styles.link}
+            className="portfolio-link"
+          >
+            <strong> View App </strong>{" "}
+          </a>
+
+          <a
+            href={props.github}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={styles.link}
+            className="portfolio-link"
+          >
+            {" "}
+            <strong> View Repo</strong>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PortfolioItemsai;
--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PortfolioItems from "../components/PortfolioItems";
+import PortfolioItemsai from "../components/PortfolioItemsai";
 import portfolioitems from "../portfolioitems.json";
+import portfolioitemsai from "../aiportfolioitems.json";
 import "../components/NavTabsstyle.css";
 const styles = {
   container: {
@@ -16,7 +18,7 @@ const styles = {
 };
 class Portfolio extends Component {
   state = {
-    portfolioitems,
+    portfolioitems, portfolioitemsai 
   };
 
   render() {
@@ -24,6 +26,28 @@ class Portfolio extends Component {
       <div className="container mb-5" style={styles.container}>
         <hr />
         <h1 style={styles.title}>Portfolio</h1>
+        <h2 style={styles.title}>AI Products and Projects</h2>
+        <p style={styles.title}>
+       Here are products/projects I built using AI tools (ex: Claude Code and Lovable).
+        </p>
+        <hr />
+        <div className="row justify-content-center">
+          {this.state.portfolioitemsai.map((item) => (
+            <PortfolioItemsai
+              key={item.id}
+              name={item.name}
+              image={item.image}
+              titleimage={item.titleimage}
+              link={item.link}
+              github={item.github}
+              shortdescription={item.shortdescription}
+              technologies={item.technologies}
+              skills={item.skills}
+            />
+          ))}
+        </div>
+        <hr />
+        <h2 style={styles.title}> Full Stack Products and Projects</h2>
         <p style={styles.title}>
           The following are projects I built, from idea to MVP. While most of my professional work has been in the B2B and B2B2C SaaS space, these products are more focused on my personal interests or areas where I saw an opportunity and had an idea to solve it. 
           Please feel free to reach out with any questions or should you see something that peeks your
